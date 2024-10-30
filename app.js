d0e01ce2b0ab86c3025ed29a176cd37415ed7058
maryam <Ofogh.maryam14@gmail.com>
2024-10-31 01:09:02 +0330

    .

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const readline=require("readline")
-const {generateMeta}=require("./controller/openAIController")
-const rl=readline.createInterface({
-input:process.stdin,
-output:process.stdout
-})
-rl.question('YouTube Video Title: \n',generateMeta)
+// const readline=require("readline")
+// const {generateMeta,generateImage}=require("./controller/openAIController")
+// const rl=readline.createInterface({
+// input:process.stdin,
+// output:process.stdout
+// })
+// // rl.question('YouTube Video Title: \n',generateMeta)
+// rl.question('Describe a YouTube thumbnail: \n',generateImage)
+
+const express=require("express")
+const {generateMeta,generateImage}=require("./controller/openAIController")
+
+const app=express()
+
+app.listen
